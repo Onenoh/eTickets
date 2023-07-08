d4fc508536d24100ccc4ee03f96a9c9d59d831d4
Okwe Anthonia <Okwe.A@Bullseyemanpower.com>
2023-07-08 20:05:29 +0100

    update

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,10 +8,16 @@ namespace eTickets.Models
     public class Actor
     {
         [Key]
-        public int ActorId { get; set; } 
+        public int ActorId { get; set; }
+
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL { get; set; }
-        public string FullName { get; set; }
-        public string Bio { get; set; }
+
+		[Display(Name = "Full Name")]
+		public string FullName { get; set; }
+
+		[Display(Name = "Biography")]
+		public string Bio { get; set; }
 
         //Relationships
         public List<Actor_Movie> Actors_Movies { get; set; }
