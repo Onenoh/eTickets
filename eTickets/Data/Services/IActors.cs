bdaae198454b492d0578100987c49ae60785e7e5
Okwe Anthonia <Okwe.A@Bullseyemanpower.com>
2023-07-15 21:08:17 +0100

    update

---
+++ eTickets/Data/Services/IActors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActors
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
